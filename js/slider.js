c3dd21923703e82537b4db213f2f011ee40f6a2a
mohammodjuwelislam <mohammodjuwelislam@gmail.com>
2024-05-28 17:31:16 +0600

    slider added

---
+++ js/slider.js
@@ -0,0 +1,35 @@
+$(document).ready(() => {
+
+    $('.slider_container').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: false,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+    });
+})
